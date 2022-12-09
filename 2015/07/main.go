cad7af4ab02df68b8ce46c4e721cd7eeac2c7f78
Ralph Schaer <ralphschaer@gmail.com>
2022-12-09 08:21:40 +0100

    Fix

--- 2015/07/main.go
+++ 2015/07/main.go
@@ -17,12 +17,12 @@ func main() {
 		log.Fatalf("reading input failed: %v", err)
 	}
 
-	part1(input)
-	//	part2(input)
+	part1and2(input, false)
+	part1and2(input, true)
 
 }
 
-func part1(input string) {
+func part1and2(input string, overrideB bool) {
 	lines := conv.SplitNewline(input)
 	re := regexp.MustCompile(`([a-z0-9]+|NOT)\s*([A-Z]*)\s*([a-z0-9]*)`)
 
@@ -30,6 +30,10 @@ func part1(input string) {
 	for _, line := range lines {
 		parts := strings.Split(line, "->")
 		instructions[strings.TrimSpace(parts[1])] = strings.TrimSpace(parts[0])
+
+		if overrideB {
+			instructions["b"] = "3176"
+		}
 	}
 
 	wires := make(map[string]uint16)
@@ -38,7 +42,7 @@ func part1(input string) {
 		for dest, src := range instructions {
 			matches := re.FindStringSubmatch(src)
 			src1, op, src2 := matches[1], matches[2], matches[3]
-			// fmt.Println(src1, op, src2)
+
 			if _, err := strconv.Atoi(src1); err != nil && strings.ToLower(src1) == src1 {
 				if _, ok := wires[src1]; !ok {
 					continue
@@ -54,31 +58,19 @@ func part1(input string) {
 			}
 
 			if src1 == "NOT" {
-				fmt.Println("NOT", src2, "->", dest)
-				wires[dest] = ^wires[src2]
+				wires[dest] = ^value(wires, src2)
 			} else {
 				switch op {
 				case "AND":
-					fmt.Println(src1, "AND", src2, "->", dest)
-					wires[dest] = wires[src1] & wires[src2]
+					wires[dest] = value(wires, src1) & value(wires, src2)
 				case "OR":
-					fmt.Println(src1, "OR", src2, "->", dest)
-					wires[dest] = wires[src1] | wires[src2]
+					wires[dest] = value(wires, src1) | value(wires, src2)
 				case "LSHIFT":
-					fmt.Println(src1, "LSHIFT", src2, "->", dest)
-					wires[dest] = wires[src1] << conv.MustAtoi(src2)
+					wires[dest] = value(wires, src1) << conv.MustAtoi(src2)
 				case "RSHIFT":
-					fmt.Println(src1, "RSHIFT", src2, "->", dest)
-					wires[dest] = wires[src1] >> conv.MustAtoi(src2)
+					wires[dest] = value(wires, src1) >> conv.MustAtoi(src2)
 				default:
-					fmt.Println(src1, "->", dest)
-					if val, err := strconv.Atoi(src1); err == nil {
-						wires[dest] = uint16(val)
-					} else {
-						fmt.Println("src1", src1)
-						fmt.Println("value", wires[src1])
-						wires[dest] = wires[src1]
-					}
+					wires[dest] = value(wires, src1)
 				}
 			}
 			updated = true
@@ -89,9 +81,18 @@ func part1(input string) {
 		}
 	}
 	fmt.Println(wires["a"])
+}
 
-	for k, v := range wires {
-		fmt.Println(k, v)
+func value(wires map[string]uint16, wireName string) uint16 {
+	if isWire(wireName) {
+		return wires[wireName]
 	}
+	return uint16(conv.MustAtoi(wireName))
+}
 
+func isWire(s string) bool {
+	if _, err := strconv.Atoi(s); err != nil && strings.ToLower(s) == s {
+		return true
+	}
+	return false
 }
