f5d6f4d58143b618a86996e624d4b8c74d9269c0
Ralph Schaer <ralphschaer@gmail.com>
2022-12-13 11:06:42 +0100

    Add

---
+++ 2015/20/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"aoc/internal/conv"
+	"aoc/internal/download"
+	"fmt"
+	"log"
+	"math"
+)
+
+func main() {
+	inputFile := "./2015/20/input.txt"
+	input, err := download.ReadInput(inputFile, 2015, 20)
+	if err != nil {
+		log.Fatalf("reading input failed: %v", err)
+	}
+
+	part1(input)
+	part2(input)
+}
+
+func part1(input string) {
+	target := conv.MustAtoi(input[:len(input)-1])
+	house := 1
+	for {
+		presents := 0
+		for _, factor := range factors(house) {
+			presents += factor * 10
+		}
+		if presents >= target {
+			break
+		}
+		house++
+	}
+
+	fmt.Println(house)
+}
+
+func part2(input string) {
+	target := conv.MustAtoi(input[:len(input)-1])
+	house := 2
+	stopAfter := 50
+	for {
+		presents := 0
+		for _, factor := range factors(house) {
+			if house/factor <= stopAfter {
+				presents += factor * 11
+			}
+		}
+		if presents >= target {
+			break
+		}
+		house++
+	}
+
+	fmt.Println(house)
+}
+
+func factors(n int) []int {
+	if n == 1 {
+		return []int{1}
+	}
+	factors := []int{1, n}
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			factors = append(factors, i)
+			if i != n/i {
+				factors = append(factors, n/i)
+			}
+		}
+	}
+	return factors
+}
