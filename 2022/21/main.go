cb46193cd1e1cffaf1298291162f2a953492e4cd
ralphschaer <ralphschaer@gmail.com>
2022-12-30 16:04:07 +0100

    Solve with z3

--- 2022/21/main.go
+++ 2022/21/main.go
@@ -4,6 +4,7 @@ import (
 	"aoc/internal/conv"
 	"aoc/internal/download"
 	"fmt"
+	"github.com/aclements/go-z3/z3"
 	"log"
 	"strings"
 )
@@ -15,119 +16,64 @@ func main() {
 		log.Fatalf("reading input failed: %v", err)
 	}
 
-	part1(input)
-	part2(input)
-
+	part1and2(input, false)
+	part1and2(input, true)
 }
 
-type monkey struct {
-	name string
-
-	leftMonkey  string
-	operation   string
-	rightMonkey string
-
-	yell int
-}
-
-var monkeys = make(map[string]*monkey)
-
-func (m *monkey) Yell() {
-	if m.leftMonkey != "" {
-		monkeys[m.leftMonkey].Yell()
-	}
-	if m.rightMonkey != "" {
-		monkeys[m.rightMonkey].Yell()
-	}
-
-	if m.operation != "" {
-		switch m.operation {
-		case "+":
-			m.yell = monkeys[m.leftMonkey].yell + monkeys[m.rightMonkey].yell
-		case "-":
-			m.yell = monkeys[m.leftMonkey].yell - monkeys[m.rightMonkey].yell
-		case "/":
-			m.yell = monkeys[m.leftMonkey].yell / monkeys[m.rightMonkey].yell
-		case "*":
-			m.yell = monkeys[m.leftMonkey].yell * monkeys[m.rightMonkey].yell
-		}
-	}
-}
-
-func part1(input string) {
+func part1and2(input string, part2 bool) {
 	lines := conv.SplitNewline(input)
-	createMonkeys(lines)
-	root := monkeys["root"]
-	root.Yell()
-	fmt.Println(root.yell)
-}
 
-func part2(input string) {
-	lines := conv.SplitNewline(input)
-	createMonkeys(lines)
+	config := z3.NewContextConfig()
+	ctx := z3.NewContext(config)
+	solver := z3.NewSolver(ctx)
 
-	human := monkeys["humn"]
-	root := monkeys["root"]
-	leftMonkey := monkeys[root.leftMonkey]
-	rightMonkey := monkeys[root.rightMonkey]
-
-	human.yell = 155
-	root.Yell()
-	y1l := leftMonkey.yell
-	y1r := rightMonkey.yell
-
-	human.yell = 153
-	root.Yell()
-	y2l := leftMonkey.yell
-	// y2r := rightMonkey.yell
-
-	var targetMonkey *monkey
-	target := 0
-	if y1l == y2l {
-		target = y1l
-		targetMonkey = monkeys[root.rightMonkey]
-	} else {
-		target = y1r
-		targetMonkey = monkeys[root.leftMonkey]
-	}
-
-	low := 1
-	high := 9000000000000
-	for low < high {
-		middle := (low + high) / 2
-		human.yell = middle
-		targetMonkey.Yell()
-		if targetMonkey.yell == target {
-			fmt.Println(middle - 1)
-			break
-		}
-		if targetMonkey.yell < target {
-			high = middle
-		} else {
-			low = middle + 1
-		}
-	}
-}
-
-func createMonkeys(lines []string) {
 	for _, line := range lines {
 		colonIx := strings.Index(line, ":")
 		name := line[:colonIx]
 		job := line[colonIx+2:]
-		yelled := 0
-		operation := ""
-		leftMonkey := ""
-		rightMonkey := ""
+		yells := 0
+		if part2 && name == "humn" {
+			ctx.IntConst("humn")
+			continue
+		}
+		d := ctx.IntConst(name)
 		if job[0] >= '0' && job[0] <= '9' {
-			yelled = conv.MustAtoi(job)
+			yells = conv.MustAtoi(job)
+			solver.Assert(d.Eq(ctx.FromInt(int64(yells), ctx.IntSort()).(z3.Int)))
 		} else {
 			splitted := strings.Fields(job)
-			leftMonkey = splitted[0]
-			operation = splitted[1]
-			rightMonkey = splitted[2]
+			operation := splitted[1]
+			left := ctx.IntConst(splitted[0])
+			right := ctx.IntConst(splitted[2])
+			if part2 && name == "root" {
+				solver.Assert(left.Eq(right))
+			} else {
+				switch operation {
+				case "+":
+					solver.Assert(d.Eq(left.Add(right)))
+				case "-":
+					solver.Assert(d.Eq(left.Sub(right)))
+				case "*":
+					solver.Assert(d.Eq(left.Mul(right)))
+				case "/":
+					solver.Assert(d.Eq(left.Div(right)))
+				}
+			}
 		}
 
-		monkeys[name] = &monkey{name: name, yell: yelled,
-			operation: operation, leftMonkey: leftMonkey, rightMonkey: rightMonkey}
+	}
+
+	ok, err := solver.Check()
+	if err != nil {
+		panic(err)
+	}
+	if ok {
+		if part2 {
+			fmt.Println(solver.Model().Eval(ctx.IntConst("humn"), true))
+		} else {
+			fmt.Println(solver.Model().Eval(ctx.IntConst("root"), true))
+		}
+	} else {
+		fmt.Println("unsat")
 	}
 }
