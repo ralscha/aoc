340110881ae4ab5bad613301bc6944b7cc5f7edc
Ralph Schaer <ralphschaer@gmail.com>
2024-11-16 11:12:50 +0100

    everybody.codes: Add solutions quest 3 + 4

---
+++ everybodycodes/quest3/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	partIandII("everybodycodes/quest3/partI.txt")
+	partIandII("everybodycodes/quest3/partII.txt")
+	partIII()
+}
+
+func partIandII(file string) {
+	input, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(input), "\n")
+	for i, l := range lines {
+		lines[i] = strings.TrimRight(l, "\r\n")
+	}
+
+	grid := make([][]rune, len(lines))
+	for i, l := range lines {
+		grid[i] = []rune(l)
+	}
+	for i, l := range grid {
+		for j, c := range l {
+			if c == '#' {
+				grid[i][j] = '1'
+			}
+		}
+	}
+
+	level := 1
+	for {
+		changed := false
+		for i := 1; i < len(grid)-1; i++ {
+			for j := 1; j < len(grid[i])-1; j++ {
+				levelString := rune(level + '0')
+				if grid[i][j] == levelString {
+					if isSurrounded4(grid, i, j, levelString, rune(level+1+'0')) {
+						grid[i][j] = rune(level + 1 + '0')
+						changed = true
+					}
+				}
+			}
+		}
+		if !changed {
+			break
+		}
+		level++
+	}
+
+	total := 0
+	for _, l := range grid {
+		for _, c := range l {
+			if c != '.' {
+				total += int(c - '0')
+			}
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func partIII() {
+	input, err := os.ReadFile("everybodycodes/quest3/partIII.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(input), "\n")
+	for i, l := range lines {
+		lines[i] = strings.TrimRight(l, "\r\n")
+	}
+
+	grid := make([][]rune, len(lines))
+	for i, l := range lines {
+		grid[i] = []rune(l)
+	}
+	for i, l := range grid {
+		for j, c := range l {
+			if c == '#' {
+				grid[i][j] = '1'
+			}
+		}
+	}
+
+	level := 1
+	for {
+		changed := false
+		for i := 1; i < len(grid)-1; i++ {
+			for j := 1; j < len(grid[i])-1; j++ {
+				levelString := rune(level + '0')
+				if grid[i][j] == levelString {
+					if isSurrounded8(grid, i, j, levelString, rune(level+1+'0')) {
+						grid[i][j] = rune(level + 1 + '0')
+						changed = true
+					}
+				}
+			}
+		}
+		if !changed {
+			break
+		}
+		level++
+	}
+
+	total := 0
+	for _, l := range grid {
+		for _, c := range l {
+			if c != '.' {
+				total += int(c - '0')
+			}
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func isSurrounded4(grid [][]rune, i, j int, surrounded ...rune) bool {
+	return (grid[i-1][j] == surrounded[0] || grid[i-1][j] == surrounded[1]) &&
+		(grid[i+1][j] == surrounded[0] || grid[i+1][j] == surrounded[1]) &&
+		(grid[i][j-1] == surrounded[0] || grid[i][j-1] == surrounded[1]) &&
+		(grid[i][j+1] == surrounded[0] || grid[i][j+1] == surrounded[1])
+}
+
+func isSurrounded8(grid [][]rune, i, j int, surrounded ...rune) bool {
+	rows := len(grid)
+	cols := len(grid[0])
+
+	directions := [][2]int{
+		{-1, 0}, {1, 0}, {0, -1}, {0, 1},
+		{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
+	}
+
+	for _, d := range directions {
+		ni, nj := i+d[0], j+d[1]
+		if ni < 0 || ni >= rows || nj < 0 || nj >= cols {
+			return false
+		}
+		if grid[ni][nj] != surrounded[0] && grid[ni][nj] != surrounded[1] {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ everybodycodes/quest4/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"aoc/internal/conv"
+	"aoc/internal/mathx"
+	"log"
+	"os"
+	"slices"
+	"sort"
+	"strings"
+)
+
+func main() {
+	partI()
+	partII()
+	partIII()
+}
+
+func partI() {
+	input := "18\n14\n7\n13\n16\n18\n16\n17\n15\n17\n13"
+
+	nails := conv.ToIntSlice(strings.Split(input, "\n"))
+	minNail := slices.Min(nails)
+	strikes := 0
+	for _, nail := range nails {
+		strikes += nail - minNail
+	}
+	println(strikes)
+}
+
+func partII() {
+	input, err := os.ReadFile("everybodycodes/quest4/partII.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(input), "\n")
+	for i, l := range lines {
+		lines[i] = strings.TrimRight(l, "\r\n")
+	}
+	if lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+
+	nails := conv.ToIntSlice(lines)
+	minNail := slices.Min(nails)
+	strikes := 0
+	for _, nail := range nails {
+		strikes += nail - minNail
+	}
+	println(strikes)
+}
+
+func partIII() {
+	input, err := os.ReadFile("everybodycodes/quest4/partIII.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(input), "\n")
+	for i, l := range lines {
+		lines[i] = strings.TrimRight(l, "\r\n")
+	}
+	if lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+
+	nails := conv.ToIntSlice(lines)
+	sortedNails := make([]int, len(nails))
+	copy(sortedNails, nails)
+	sort.Ints(sortedNails)
+
+	medianHeight := sortedNails[len(sortedNails)/2]
+
+	strikes := 0
+	for _, nail := range nails {
+		strikes += mathx.Abs(nail - medianHeight)
+	}
+	println(strikes)
+
+}
