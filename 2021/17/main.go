9328191f12806395e876d51589e2553a99fee8ea
Ralph Schaer <ralphschaer@gmail.com>
2024-12-08 09:44:42 +0100

    Cleanup

--- 2021/17/main.go
+++ 2021/17/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"aoc/internal/container"
 	"aoc/internal/conv"
 	"aoc/internal/download"
+	"aoc/internal/gridutil"
 	"fmt"
 	"log"
 	"strings"
 )
+
+type targetArea struct {
+	xMin, xMax, yMin, yMax int
+}
 
 func main() {
 	input, err := download.ReadInput(2021, 17)
@@ -18,96 +24,89 @@ func main() {
 }
 
 func part1and2(input string) {
+	target := parseInput(input)
+	validVelocities := findValidVelocities(target)
+
+	// Find highest Y position
+	highestY := 0
+	for _, vel := range validVelocities.Values() {
+		_, maxY := simulate(vel, target)
+		if maxY > highestY {
+			highestY = maxY
+		}
+	}
+
+	fmt.Println("Part 1", highestY)
+	fmt.Println("Part 2", validVelocities.Len())
+}
+
+func parseInput(input string) targetArea {
 	lines := conv.SplitNewline(input)
-	splitted := strings.Fields(lines[0])
-	xStr := splitted[2]
-	xStr = xStr[2 : len(xStr)-1]
-	yStr := splitted[3]
-	yStr = yStr[2:]
+	parts := strings.Fields(lines[0])
 
-	xSplitted := strings.Split(xStr, "..")
-	ySplitted := strings.Split(yStr, "..")
-	xMin := conv.MustAtoi(xSplitted[0])
-	xMax := conv.MustAtoi(xSplitted[1])
-	yMin := conv.MustAtoi(ySplitted[0])
-	yMax := conv.MustAtoi(ySplitted[1])
+	// Parse X range
+	xStr := strings.TrimPrefix(parts[2], "x=")
+	xStr = strings.TrimSuffix(xStr, ",")
+	xRange := strings.Split(xStr, "..")
+	xMin := conv.MustAtoi(xRange[0])
+	xMax := conv.MustAtoi(xRange[1])
 
-	target := area{xMin: xMin, xMax: xMax, yMin: yMin, yMax: yMax}
-	highestY := findHighestY(target)
-	fmt.Println(highestY)
+	// Parse Y range
+	yStr := strings.TrimPrefix(parts[3], "y=")
+	yRange := strings.Split(yStr, "..")
+	yMin := conv.MustAtoi(yRange[0])
+	yMax := conv.MustAtoi(yRange[1])
 
-	count := findAllVelocities(target)
-	fmt.Println(count)
+	return targetArea{xMin: xMin, xMax: xMax, yMin: yMin, yMax: yMax}
 }
 
-type point struct {
-	x, y int
+func findValidVelocities(target targetArea) *container.Set[gridutil.Direction] {
+	validVelocities := container.NewSet[gridutil.Direction]()
+
+	// We can determine reasonable bounds for velocity search:
+	// - X velocity must be positive and not overshoot target in first step
+	// - Y velocity must not undershoot target after first step down
+	for dx := 0; dx <= target.xMax; dx++ {
+		for dy := target.yMin; dy <= -target.yMin; dy++ {
+			vel := gridutil.Direction{Row: dy, Col: dx}
+			if hit, _ := simulate(vel, target); hit {
+				validVelocities.Add(vel)
+			}
+		}
+	}
+
+	return validVelocities
 }
 
-type velocity struct {
-	dx, dy int
-}
+func simulate(velocity gridutil.Direction, target targetArea) (bool, int) {
+	pos := gridutil.Coordinate{Row: 0, Col: 0}
+	vel := velocity
+	maxY := pos.Row
 
-type area struct {
-	xMin, xMax, yMin, yMax int
-}
+	for pos.Col <= target.xMax && pos.Row >= target.yMin {
+		// Update position
+		pos.Col += vel.Col
+		pos.Row += vel.Row
 
-func simulate(v velocity, target area) (bool, int) {
-	position := point{0, 0}
-	maxY := position.y
-
-	for position.x <= target.xMax && position.y >= target.yMin {
-		position.x += v.dx
-		position.y += v.dy
-
-		if position.y > maxY {
-			maxY = position.y
+		// Track maximum height
+		if pos.Row > maxY {
+			maxY = pos.Row
 		}
 
-		if v.dx > 0 {
-			v.dx--
-		} else if v.dx < 0 {
-			v.dx++
+		// Update velocity
+		if vel.Col > 0 {
+			vel.Col--
+		} else if vel.Col < 0 {
+			vel.Col++
 		}
+		vel.Row--
 
-		v.dy--
-
-		if position.x >= target.xMin && position.x <= target.xMax && position.y >= target.yMin && position.y <= target.yMax {
+		// Check if we hit the target
+		if pos.Col >= target.xMin && pos.Col <= target.xMax &&
+			pos.Row >= target.yMin && pos.Row <= target.yMax {
 			return true, maxY
 		}
 	}
 
 	return false, maxY
-}
-
-func findHighestY(target area) int {
-	highestY := 0
-
-	for dx := 0; dx <= target.xMax; dx++ {
-		for dy := target.yMin; dy <= -target.yMin; dy++ {
-			v := velocity{dx, dy}
-			success, maxY := simulate(v, target)
-			if success && maxY > highestY {
-				highestY = maxY
-			}
-		}
-	}
-
-	return highestY
-}
-
-func findAllVelocities(target area) int {
-	count := 0
-
-	for dx := 0; dx <= target.xMax; dx++ {
-		for dy := target.yMin; dy <= -target.yMin; dy++ {
-			v := velocity{dx, dy}
-			hit, _ := simulate(v, target)
-			if hit {
-				count++
-			}
-		}
-	}
-
-	return count
 }
