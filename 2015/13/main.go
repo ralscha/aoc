ba467c520c8c5292039bdc40aca9ee0686a5c547
Ralph Schaer <ralphschaer@gmail.com>
2024-12-10 15:07:11 +0100

    Cleanup

--- 2015/13/main.go
+++ 2015/13/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc/internal/conv"
 	"aoc/internal/download"
+	"aoc/internal/graphutil"
 	"aoc/internal/mathx"
 	"fmt"
 	"log"
@@ -19,31 +20,29 @@ func main() {
 	part2(input)
 }
 
-type person struct {
-	neighbours map[string]int
-}
-
 func part1(input string) {
 	lines := conv.SplitNewline(input)
-	persons := createPersons(lines)
-	maxHappiness(persons)
+	graph := createGraph(lines)
+	maxHappiness(graph)
 }
 
 func part2(input string) {
 	lines := conv.SplitNewline(input)
-	persons := createPersons(lines)
-	persons["me"] = &person{neighbours: make(map[string]int)}
-	for name := range persons {
-		if name != "me" {
-			persons[name].neighbours["me"] = 0
-			persons["me"].neighbours[name] = 0
-		}
+	graph := createGraph(lines)
+	
+	// Add "me" to the graph with 0 happiness to/from everyone
+	for _, node := range graph.GetNeighbors("") { // Empty string gets all nodes
+		graph.AddNode("me", nil)
+		graph.AddEdge("me", node.ID, 0)
+		graph.AddEdge(node.ID, "me", 0)
 	}
-	maxHappiness(persons)
+	
+	maxHappiness(graph)
 }
 
-func createPersons(lines []string) map[string]*person {
-	persons := make(map[string]*person)
+func createGraph(lines []string) *graphutil.Graph {
+	graph := graphutil.NewGraph()
+	
 	for _, line := range lines {
 		splitted := strings.Fields(line)
 		name := splitted[0]
@@ -53,21 +52,20 @@ func createPersons(lines []string) map[string]*person {
 		}
 		neighbour := splitted[10]
 		neighbour = neighbour[:len(neighbour)-1]
-		if p, ok := persons[name]; ok {
-			p.neighbours[neighbour] = gainLoseValue
-		} else {
-			persons[name] = &person{
-				neighbours: map[string]int{neighbour: gainLoseValue},
-			}
-		}
+		
+		graph.AddNode(name, nil)
+		graph.AddNode(neighbour, nil)
+		graph.AddEdge(name, neighbour, gainLoseValue)
 	}
-	return persons
+	
+	return graph
 }
 
-func maxHappiness(persons map[string]*person) {
-	names := make([]string, 0, len(persons))
-	for name := range persons {
-		names = append(names, name)
+func maxHappiness(graph *graphutil.Graph) {
+	// Get all names (nodes)
+	names := make([]string, 0)
+	for _, node := range graph.GetNeighbors("") { // Empty string gets all nodes
+		names = append(names, node.ID)
 	}
 
 	perms := mathx.Permutations(names)
@@ -75,10 +73,16 @@ func maxHappiness(persons map[string]*person) {
 	for _, perm := range perms {
 		happiness := 0
 		for i, name := range perm {
-			neighbour1 := perm[(i-1+len(perm))%len(perm)]
-			neighbour2 := perm[(i+1)%len(perm)]
-			happiness += persons[name].neighbours[neighbour1]
-			happiness += persons[name].neighbours[neighbour2]
+			// Get happiness values between current person and their neighbors in the seating
+			neighbor1 := perm[(i-1+len(perm))%len(perm)]
+			neighbor2 := perm[(i+1)%len(perm)]
+			
+			// Add happiness in both directions (person -> neighbor and neighbor -> person)
+			for _, n := range graph.GetNeighbors(name) {
+				if n.ID == neighbor1 || n.ID == neighbor2 {
+					happiness += n.Edge.Weight
+				}
+			}
 		}
 		if happiness > maxHappiness {
 			maxHappiness = happiness
