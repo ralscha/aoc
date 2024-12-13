9a254d721ce9062ca68a8e0cb36288b21cf883f9
Ralph Schaer <ralphschaer@gmail.com>
2024-12-13 19:31:38 +0100

    Cleanup

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"aoc/internal/conv"
 	"aoc/internal/download"
+	"aoc/internal/stringutil"
 	"fmt"
 	"log"
 	"strconv"
-	"strings"
 	"unicode"
 )
 
@@ -24,17 +24,15 @@ func part1(input string) {
 	lines := conv.SplitNewline(input)
 	sum := 0
 	for _, line := range lines {
-		firstDigit := ""
-		lastDigit := ""
+		var digits []string
 		for _, c := range line {
 			if unicode.IsDigit(c) {
-				if firstDigit == "" {
-					firstDigit = string(c)
-				}
-				lastDigit = string(c)
+				digits = append(digits, string(c))
 			}
 		}
-		sum = sum + conv.MustAtoi(firstDigit+lastDigit)
+		if len(digits) > 0 {
+			sum += conv.MustAtoi(digits[0] + digits[len(digits)-1])
+		}
 	}
 	fmt.Println(sum)
 }
@@ -47,51 +45,49 @@ func part2(input string) {
 	lines := conv.SplitNewline(input)
 	sum := 0
 	for _, line := range lines {
-		firstDigit := ""
-		lastDigit := ""
+		// Find all digit positions
+		var digitPositions []struct {
+			pos   int
+			value string
+		}
 
-		indexOfFirstDigit := -1
-		indexOfLastDigit := len(line)
+		// Find numeric digits
 		for i, c := range line {
 			if unicode.IsDigit(c) {
-				firstDigit = string(c)
-				indexOfFirstDigit = i
-				break
+				digitPositions = append(digitPositions, struct {
+					pos   int
+					value string
+				}{i, string(c)})
 			}
 		}
 
-		for i := len(line) - 1; i >= 0; i-- {
-			if unicode.IsDigit(rune(line[i])) {
-				lastDigit = string(line[i])
-				indexOfLastDigit = i
-				break
+		// Find word digits
+		for i, word := range numbers {
+			// Use stringutil.FindAllOccurrences to find all instances of each number word
+			positions := stringutil.FindAllOccurrences(line, word)
+			for _, pos := range positions {
+				digitPositions = append(digitPositions, struct {
+					pos   int
+					value string
+				}{pos, strconv.Itoa(i)})
 			}
 		}
 
-		if indexOfFirstDigit > 0 {
-			for i := 0; i < indexOfFirstDigit; i++ {
-				for j, number := range numbers {
-					if strings.HasPrefix(line[i:], number) {
-						firstDigit = strconv.Itoa(j)
-						indexOfFirstDigit = i
-						break
-					}
+		if len(digitPositions) > 0 {
+			// Sort positions by their index to find first and last
+			firstPos := digitPositions[0]
+			lastPos := digitPositions[0]
+			for _, pos := range digitPositions {
+				if pos.pos < firstPos.pos {
+					firstPos = pos
+				}
+				if pos.pos > lastPos.pos {
+					lastPos = pos
 				}
 			}
-		}
 
-		if indexOfLastDigit < len(line)-1 {
-			for i := indexOfLastDigit + 1; i < len(line); i++ {
-				for j, number := range numbers {
-					if strings.HasPrefix(line[i:], number) {
-						lastDigit = strconv.Itoa(j)
-						indexOfLastDigit = i + len(number) - 1
-						break
-					}
-				}
-			}
+			sum += conv.MustAtoi(firstPos.value + lastPos.value)
 		}
-		sum = sum + conv.MustAtoi(firstDigit+lastDigit)
 	}
 	fmt.Println(sum)
 }
