cef7e02ae4a8077a5493e40c45023711cc771bc4
Ralph Schaer <ralphschaer@gmail.com>
2024-12-13 19:14:59 +0100

    Cleanup

--- 2022/18/main.go
+++ 2022/18/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"aoc/internal/container"
 	"aoc/internal/conv"
 	"aoc/internal/download"
-	"container/list"
+	"aoc/internal/gridutil"
 	"fmt"
 	"log"
 	"math"
@@ -20,59 +21,55 @@ func main() {
 	part2(input)
 }
 
-var directions = []point3d{
-	{-1, 0, 0},
-	{1, 0, 0},
-	{0, -1, 0},
-	{0, 1, 0},
-	{0, 0, -1},
-	{0, 0, 1},
-}
-
-type point3d struct {
-	x, y, z int
+var directions = []gridutil.Coordinate3D{
+	{X: -1, Y: 0, Z: 0},
+	{X: 1, Y: 0, Z: 0},
+	{X: 0, Y: -1, Z: 0},
+	{X: 0, Y: 1, Z: 0},
+	{X: 0, Y: 0, Z: -1},
+	{X: 0, Y: 0, Z: 1},
 }
 
 func part1(input string) {
 	lines := conv.SplitNewline(input)
 
-	cubes := createCubs(lines)
+	cubes := createCubes(lines)
 	surfaceArea := 0
-	for cube := range cubes {
+	for _, cube := range cubes.Values() {
 		for _, d := range directions {
-			if _, ok := cubes[point3d{cube.x + d.x, cube.y + d.y, cube.z + d.z}]; !ok {
+			neighbor := cube.Add(d)
+			if !cubes.Contains(neighbor) {
 				surfaceArea++
 			}
 		}
-
 	}
 	fmt.Println(surfaceArea)
 }
 
 func part2(input string) {
 	lines := conv.SplitNewline(input)
-	cubes := createCubs(lines)
+	cubes := createCubes(lines)
 
 	minX, minY, minZ := math.MaxInt, math.MaxInt, math.MaxInt
 	maxX, maxY, maxZ := 0, 0, 0
-	for cube := range cubes {
-		if cube.x < minX {
-			minX = cube.x
+	for _, cube := range cubes.Values() {
+		if cube.X < minX {
+			minX = cube.X
 		}
-		if cube.y < minY {
-			minY = cube.y
+		if cube.Y < minY {
+			minY = cube.Y
 		}
-		if cube.z < minZ {
-			minZ = cube.z
+		if cube.Z < minZ {
+			minZ = cube.Z
 		}
-		if cube.x > maxX {
-			maxX = cube.x
+		if cube.X > maxX {
+			maxX = cube.X
 		}
-		if cube.y > maxY {
-			maxY = cube.y
+		if cube.Y > maxY {
+			maxY = cube.Y
 		}
-		if cube.z > maxZ {
-			maxZ = cube.z
+		if cube.Z > maxZ {
+			maxZ = cube.Z
 		}
 	}
 
@@ -88,37 +85,33 @@ func part2(input string) {
 	maxY++
 	maxZ++
 
-	queue := list.New()
-	start := point3d{minX, minY, minZ}
-	queue.PushBack(start)
-	visited := make(map[point3d]struct{})
-	visited[start] = struct{}{}
+	queue := container.NewQueue[gridutil.Coordinate3D]()
+	start := gridutil.Coordinate3D{X: minX, Y: minY, Z: minZ}
+	queue.Push(start)
+	visited := container.NewSet[gridutil.Coordinate3D]()
+	visited.Add(start)
 
-	for queue.Len() > 0 {
-		s := queue.Front()
-		cube := s.Value.(point3d)
-		queue.Remove(s)
+	for !queue.IsEmpty() {
+		cube := queue.Pop()
 
 		for _, d := range directions {
-			neighbour := point3d{cube.x + d.x, cube.y + d.y, cube.z + d.z}
-			if _, ok := visited[neighbour]; !ok {
-				if _, ok := cubes[neighbour]; !ok {
-					if neighbour.x >= minX && neighbour.x <= maxX &&
-						neighbour.y >= minY && neighbour.y <= maxY &&
-						neighbour.z >= minZ && neighbour.z <= maxZ {
-						visited[neighbour] = struct{}{}
-						queue.PushBack(neighbour)
-					}
+			neighbor := cube.Add(d)
+			if !visited.Contains(neighbor) && !cubes.Contains(neighbor) {
+				if neighbor.X >= minX && neighbor.X <= maxX &&
+					neighbor.Y >= minY && neighbor.Y <= maxY &&
+					neighbor.Z >= minZ && neighbor.Z <= maxZ {
+					visited.Add(neighbor)
+					queue.Push(neighbor)
 				}
 			}
 		}
 	}
 
 	surfaceAreaVisited := 0
-	for cube := range visited {
+	for _, cube := range visited.Values() {
 		for _, d := range directions {
-			neighbour := point3d{cube.x + d.x, cube.y + d.y, cube.z + d.z}
-			if _, ok := visited[neighbour]; !ok {
+			neighbor := cube.Add(d)
+			if !visited.Contains(neighbor) {
 				surfaceAreaVisited++
 			}
 		}
@@ -127,12 +120,12 @@ func part2(input string) {
 	fmt.Println(surfaceAreaVisited - boundingBoxSurfaceArea)
 }
 
-func createCubs(lines []string) map[point3d]struct{} {
-	cubes := make(map[point3d]struct{})
+func createCubes(lines []string) *container.Set[gridutil.Coordinate3D] {
+	cubes := container.NewSet[gridutil.Coordinate3D]()
 	for _, line := range lines {
 		splitted := strings.Split(line, ",")
 		x, y, z := conv.MustAtoi(splitted[0]), conv.MustAtoi(splitted[1]), conv.MustAtoi(splitted[2])
-		cubes[point3d{x, y, z}] = struct{}{}
+		cubes.Add(gridutil.Coordinate3D{X: x, Y: y, Z: z})
 	}
 	return cubes
 }
