0d590d824c1240522d93403a0edc9ffc845206c3
ralphschaer <ralphschaer@gmail.com>
2024-12-19 12:28:56 +0100

    Add solution 2016/20

---
+++ 2016/20/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"aoc/internal/conv"
+	"aoc/internal/download"
+	"aoc/internal/rangeutil"
+	"fmt"
+	"log"
+	"strings"
+)
+
+func parseInput(input string) []rangeutil.Range {
+	lines := conv.SplitNewline(input)
+	ranges := make([]rangeutil.Range, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, "-")
+		ranges[i] = rangeutil.NewRange(conv.MustAtoi(parts[0]), conv.MustAtoi(parts[1]))
+	}
+	return ranges
+}
+
+func main() {
+	input, err := download.ReadInput(2016, 20)
+	if err != nil {
+		log.Fatalf("reading input failed: %v", err)
+	}
+
+	part1(input)
+	part2(input)
+}
+
+func part1(input string) {
+	ranges := parseInput(input)
+	mergedRanges := rangeutil.Merge(ranges)
+	for i := 0; i < len(mergedRanges)-1; i++ {
+		if mergedRanges[i].End+1 < mergedRanges[i+1].Start {
+			fmt.Println("Part 1", mergedRanges[i].End+1)
+			return
+		}
+	}
+}
+
+func part2(input string) {
+	ranges := parseInput(input)
+	mergedRanges := rangeutil.Merge(ranges)
+
+	allowed := 0
+	for i := 0; i < len(mergedRanges)-1; i++ {
+		allowed += mergedRanges[i+1].Start - (mergedRanges[i].End + 1)
+	}
+
+	fmt.Println("Part 2", allowed)
+}
