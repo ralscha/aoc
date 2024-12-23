4cdf6b3770638081a78e279e709cf441949379f4
Ralph Schaer <ralphschaer@gmail.com>
2024-12-23 09:36:05 +0100

    Cleanup

--- internal/stringutil/stringutil.go
+++ internal/stringutil/stringutil.go
@@ -1,14 +1,15 @@
 package stringutil
 
-import "strings"
+import (
+	"slices"
+	"strings"
+)
 
 // Reverse returns a string with its characters in reverse order.
 // For example: Reverse("hello") returns "olleh".
 func Reverse(s string) string {
 	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
+	slices.Reverse(runes)
 	return string(runes)
 }
 
