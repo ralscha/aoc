c7c1f00754ce7c18d570a7dedfb90878e0615490
ralphschaer <ralphschaer@gmail.com>
2024-12-26 04:41:08 +0100

    Add more solutions

--- 2018/23/main.go
+++ 2018/23/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"aoc/internal/container"
 	"aoc/internal/conv"
 	"aoc/internal/download"
 	"aoc/internal/gridutil"
 	"fmt"
 	"log"
+	"slices"
 )
 
 type nanobot struct {
@@ -61,5 +63,101 @@ func part1(input string) {
 }
 
 func part2(input string) {
-	fmt.Println("Part 2", "not yet implemented")
+	nanobots := parseNanobots(input)
+
+	neighbors := make(map[int][]int)
+	for i, bot0 := range nanobots {
+		for j, bot1 := range nanobots {
+			if i == j || bot0.pos.ManhattanDistance(bot1.pos) > bot0.radius+bot1.radius {
+				continue
+			}
+			neighbors[j] = append(neighbors[j], i)
+		}
+	}
+	cliques := bronKerbosch(neighbors)
+	clique := cliques[0]
+
+	var maxMin int
+	var origin gridutil.Coordinate3D
+	for _, i := range clique {
+		bot := nanobots[i]
+		d := origin.ManhattanDistance(bot.pos) - bot.radius
+		if d > maxMin {
+			maxMin = d
+		}
+	}
+	fmt.Println("Part 2", maxMin)
+}
+
+func bronKerbosch(g map[int][]int) [][]int {
+	p := container.NewSet[int]()
+	for v := range g {
+		p.Add(v)
+	}
+	state := &bkState{g: g}
+	r := container.NewSet[int]()
+	x := container.NewSet[int]()
+	bronKerbosch1(state, r, p, x)
+	return state.maxCliques
+}
+
+type bkState struct {
+	g          map[int][]int
+	maxCliques [][]int
+}
+
+func bronKerbosch1(state *bkState, r, p, x *container.Set[int]) {
+	if p.Len() == 0 && x.Len() == 0 {
+		if len(state.maxCliques) > 0 && len(state.maxCliques[0]) > r.Len() {
+			return
+		}
+		if len(state.maxCliques) > 0 && len(state.maxCliques[0]) < r.Len() {
+			state.maxCliques = nil
+		}
+		clique := make([]int, 0, r.Len())
+		for _, v := range r.Values() {
+			clique = append(clique, v)
+		}
+		slices.Sort(clique)
+		state.maxCliques = append(state.maxCliques, clique)
+		return
+	}
+	u := -1
+	if p.Len() > 0 {
+		for _, v := range p.Values() {
+			u = v
+			break
+		}
+	} else {
+		for _, v := range x.Values() {
+			u = v
+			break
+		}
+	}
+	nu := state.g[u]
+	nuSet := container.NewSet[int]()
+	for _, uu := range nu {
+		nuSet.Add(uu)
+	}
+	for _, v := range p.Values() {
+		if nuSet.Contains(v) {
+			continue
+		}
+		ns := state.g[v]
+		p1 := container.NewSet[int]()
+		x1 := container.NewSet[int]()
+		for _, n := range ns {
+			if p.Contains(n) {
+				p1.Add(n)
+			}
+			if x.Contains(n) {
+				x1.Add(n)
+			}
+		}
+		r.Add(v)
+		bronKerbosch1(state, r, p1, x1)
+		r.Remove(v)
+		p.Remove(v)
+		x.Add(v)
+	}
 }
