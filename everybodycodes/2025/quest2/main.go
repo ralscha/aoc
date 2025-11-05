ee7b82ae12d443c433ce8b149069169f96175473
Ralph Schaer <ralphschaer@gmail.com>
2025-11-05 11:58:03 +0100

    everybody codes quest 2

---
+++ everybodycodes/2025/quest2/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	partI()
+	partII()
+	partIII()
+}
+
+func partI() {
+	input := "A=[158,55]"
+
+	var ax, ay int
+	_, err := fmt.Sscanf(input, "A=[%d,%d]", &ax, &ay)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rx, ry := 0, 0
+
+	for range 3 {
+		rxNew := rx*rx - ry*ry
+		ryNew := rx*ry + ry*rx
+		rx, ry = rxNew, ryNew
+
+		rx /= 10
+		ry /= 10
+
+		rx += ax
+		ry += ay
+	}
+
+	fmt.Printf("[%d,%d]\n", rx, ry)
+}
+
+func partII() {
+	input := "A=[-79067,14068]"
+	var ax, ay int
+	_, err := fmt.Sscanf(input, "A=[%d,%d]", &ax, &ay)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engravedCount := solve(ax, ay, 101, 10)
+	fmt.Println(engravedCount)
+}
+
+func partIII() {
+	input := "A=[-79067,14068]"
+	var ax, ay int
+	_, err := fmt.Sscanf(input, "A=[%d,%d]", &ax, &ay)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engravedCount := solve(ax, ay, 1001, 1)
+	fmt.Println(engravedCount)
+}
+
+func solve(ax, ay, gridSize, step int) int {
+	engravedCount := 0
+
+	for i := range gridSize {
+		for j := range gridSize {
+			px := ax + i*step
+			py := ay + j*step
+
+			rx, ry := 0, 0
+			engrave := true
+			for range 100 {
+				rxNew := rx*rx - ry*ry
+				ryNew := rx*ry + ry*rx
+				rx, ry = rxNew, ryNew
+
+				rx /= 100000
+				ry /= 100000
+
+				rx += px
+				ry += py
+
+				if rx > 1000000 || rx < -1000000 || ry > 1000000 || ry < -1000000 {
+					engrave = false
+					break
+				}
+			}
+			if engrave {
+				engravedCount++
+			}
+		}
+	}
+
+	return engravedCount
+}
